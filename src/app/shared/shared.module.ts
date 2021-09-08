f55fc514c6465e267bb3445bd1a90826fc43b90e
Valdenir Oliveira <58260303+Valdenir-Oliveira@users.noreply.github.com>
2021-09-08 09:57:31 -0300

    Implementando animações 2

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,12 +8,13 @@ import { RatingComponent } from "./rating/rating.component";
 import { OrderService } from "app/order/order.service";
 import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
 import { RestaurantService } from "app/restaurants/restaurant.service";
+import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 
 
 @NgModule({
-    declarations: [InputComponent, RadioComponent,RatingComponent],
+    declarations: [InputComponent, RadioComponent,RatingComponent, SnackbarComponent],
     imports: [CommonModule, FormsModule, ReactiveFormsModule],
-    exports: [InputComponent, RadioComponent,RatingComponent,
+    exports: [InputComponent, RadioComponent,RatingComponent, SnackbarComponent,
          CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class SharedModule {
