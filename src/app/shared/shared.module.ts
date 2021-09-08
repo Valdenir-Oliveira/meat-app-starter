ac0b915152d0df9b6e1cd34aca56a1721992ca26
Valdenir Oliveira <58260303+Valdenir-Oliveira@users.noreply.github.com>
2021-09-08 08:38:12 -0300

    Adicionando serviços em módulo compartilhado e pré-carregamento de todos os módulos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from "@angular/core";
+import { NgModule, ModuleWithProviders } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { InputComponent } from "./input/input.component";
 import { RadioComponent } from "./radio/radio.component";
 import { RatingComponent } from "./rating/rating.component";
+import { OrderService } from "app/order/order.service";
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantService } from "app/restaurants/restaurant.service";
 
 
 @NgModule({
@@ -13,4 +16,11 @@ import { RatingComponent } from "./rating/rating.component";
     exports: [InputComponent, RadioComponent,RatingComponent,
          CommonModule, FormsModule, ReactiveFormsModule]
 })
-export class SharedModule {}
+export class SharedModule {
+    static forRoot(): ModuleWithProviders{
+        return{
+            ngModule: SharedModule,
+            providers:[ShoppingCartService, RestaurantService, OrderService]
+        }
+    }
+}
