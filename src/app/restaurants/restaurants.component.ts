ed600355021194b0ab47c03274d0f4171b299bba
Valdenir Oliveira <58260303+Valdenir-Oliveira@users.noreply.github.com>
2021-09-09 08:51:38 -0300

    Implementando Reactive Forms e operadores debounceTime e distinctUntilChanged

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantService } from './restaurant.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { FormBuilder,FormControl, FormGroup } from '@angular/forms';
+import { switchMap } from 'rxjs/operator/switchMap';
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
 
 @Component({
   selector: 'mt-restaurants',
@@ -31,9 +36,26 @@ export class RestaurantsComponent implements OnInit {
 
   restaurants: Restaurant[] 
 
-  constructor(private restaurantsService: RestaurantService) { }
+  searchForm: FormGroup
+  searchControl: FormControl
+
+  constructor(private restaurantsService: RestaurantService,
+              private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.searchControl = this.fb.control('')
+    this.searchForm = this.fb.group({
+      searchControl: this.searchControl
+    })
+
+    this.searchControl.valueChanges
+    .debounceTime(500)
+    .distinctUntilChanged()
+        .switchMap(searchTerm => 
+          this.restaurantsService.restaurants(searchTerm))
+        .subscribe(restaurants => this.restaurants = restaurants)
+
     this.restaurantsService.restaurants()
     .subscribe(restaurants => this.restaurants = restaurants)
   }
